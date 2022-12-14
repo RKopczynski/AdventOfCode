4e4d1e0d0ac014a8a9b66abd412fce069ebd6b14
Rafał Kopczyński <rafal.kopczynski@rockwool.com>
2022-12-14 17:25:19 +0100

    Day 1 - part 1 done.

---
+++ Day1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "input"));
+
+var biggestElf = 0;
+var newElf = 0;
+
+foreach (var line in lines)
+{
+    if (!string.IsNullOrWhiteSpace(line))
+    {
+        newElf += int.Parse(line);
+        continue;
+    }
+
+    if (biggestElf < newElf)
+    {
+        biggestElf = newElf;
+    }
+
+    newElf = 0;
+}
+
+Console.WriteLine("Biggest elf carry: {0}", biggestElf);
