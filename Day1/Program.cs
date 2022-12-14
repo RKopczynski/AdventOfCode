71d898e719616b29b1cb0007b0f230926dbc3416
Rafał Kopczyński <rafal.kopczynski@rockwool.com>
2022-12-14 17:47:30 +0100

    Day 1 - part 2 done.

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,24 +1,68 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
 var lines = File.ReadAllLines(Path.Combine(Environment.CurrentDirectory, "input"));
 
-var biggestElf = 0;
-var newElf = 0;
+GetTopElf(lines);
+GetTop3Elves(lines);
 
-foreach (var line in lines)
+void GetTop3Elves(IEnumerable<string> lines)
 {
-    if (!string.IsNullOrWhiteSpace(line))
+    var elves = new List<int>();
+    using var enumerator = lines.GetEnumerator();
+    
+    while (GetNextElf(enumerator, out var elf))
     {
-        newElf += int.Parse(line);
-        continue;
+        elves.Add(elf);
     }
 
-    if (biggestElf < newElf)
-    {
-        biggestElf = newElf;
-    }
+    var top3Elves = elves.OrderByDescending(e => e)
+        .Take(3)
+        .Aggregate((a, b) => a + b);
+    
+    Console.WriteLine("Top 3 elves weight {0}", top3Elves);
 
-    newElf = 0;
+    static bool GetNextElf(IEnumerator<string> elfEnumerator, out int elf)
+    {
+        elf = 0;
+        var next = string.Empty;
+
+        do
+        {
+            if (!elfEnumerator.MoveNext())
+                return false;
+
+            next = elfEnumerator.Current;
+            if (string.IsNullOrWhiteSpace(next))
+            {
+                break;
+            }
+
+            elf += int.Parse(next);
+        } while (true);
+
+        return true;
+    }
 }
 
-Console.WriteLine("Biggest elf carry: {0}", biggestElf);
+void GetTopElf(IEnumerable<string> lines)
+{
+    var biggestElf = 0;
+    var newElf = 0;
+
+    foreach (var line in lines)
+    {
+        if (!string.IsNullOrWhiteSpace(line))
+        {
+            newElf += int.Parse(line);
+            continue;
+        }
+
+        if (biggestElf < newElf)
+        {
+            biggestElf = newElf;
+        }
+
+        newElf = 0;
+    }
+
+    Console.WriteLine("Biggest elf carry: {0}", biggestElf);
+}
